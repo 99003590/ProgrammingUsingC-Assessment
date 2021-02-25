f3455497165d20802cf10d499f65e3bc410e8f12
99003590 <78849866+99003590@users.noreply.github.com>
2021-02-25 17:31:05 +0530

    Update GourmetShop.cs

--- ProblemThree_CompleteSolution_BL/GourmetShop.cs
+++ ProblemThree_CompleteSolution_BL/GourmetShop.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +10,10 @@ namespace ProblemThree_CompleteSolution_BL
     {          
         public abstract double PayPerPiece(int quantity);
         public abstract double PayPerWeight(int weight);
+        public int ItemCode { get; set; }
+        public String ItemName { get; set; }
+        public int ItemPrice { get; set; }
+        
 
     }
 }
